997f49b247d14446ce79308a7c12c1dfc450abf0
Rebecca Fransson <rebecca@kollektiva.se>
2017-07-17 22:52:36 +0200

    front page

--- CK-horseLife/src/components/Home.vue
+++ CK-horseLife/src/components/Home.vue
@@ -1,16 +1,12 @@
 <template>
   <div id="home">
-    <input type="text" v-model="title">
-    <h1>{{title}}</h1>
-    <ul>
-      <li v-for="item in items">{{ item.title }}</li>
-    </ul>
-    <input type="text" v-model="user.firstname">
-    <input type="text" v-model="user.lastname">
-    <h3>{{fullname}}</h3>
-    <p>end</p>
-    <h2>{{msg}}</h2>
-    <router-link v-bind:to="'add'">add new blogpost</router-link>
+    <div id="navigation">
+      <div class="navItem"><router-link v-bind:to="'add'"><span>Gården</span></router-link></div>
+      <div class="navItem"><router-link v-bind:to="'add'"><span>Hästar</span></router-link></div>
+      <div class="navItem"><router-link v-bind:to="'add'"><span>Uppfödning</span></router-link></div>
+      <div class="navItem"><router-link v-bind:to="'add'"><span>Medryttare</span></router-link></div>
+      <div class="navItem"><router-link v-bind:to="'add'"><span>Inackoderingar</span></router-link></div>
+    </div>
   </div>
 </template>
 
@@ -49,8 +45,46 @@
 </script>
 
 <style>
-#home{
-  background-color: hotpink;
+@import url('https://fonts.googleapis.com/css?family=Roboto');
+html{
+  background: url("../../static/animal.jpg") no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
 }
-
+body{
+  margin: 0;
+  padding: 0;
+}
+#navigation{
+  background-color: rgba(169, 115, 100, 0.77);
+  padding: 25px;
+  font-size: 27px;
+  box-shadow: rgba(125, 101, 94, 0.61) 2px 2px 5px 0px;
+}
+#navigation .navItem{
+  float: left;
+}
+#navigation a{
+  color: #fff2ed;
+  text-decoration: none;
+  font-family: 'Roboto', sans-serif;
+  padding: 15px;
+}
+#navigation a:hover{
+}
 </style>
+<!--
+    <input type="text" v-model="title">
+    <h1>{{title}}</h1>
+    <ul>
+      <li v-for="item in items">{{ item.title }}</li>
+    </ul>
+    <input type="text" v-model="user.firstname">
+    <input type="text" v-model="user.lastname">
+    <h3>{{fullname}}</h3>
+    <p>end</p>
+    <h2>{{msg}}</h2>
+    <router-link v-bind:to="'add'">add new blogpost</router-link>
+-->
