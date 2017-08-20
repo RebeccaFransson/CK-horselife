4a2e9ed310309517aad3c6194e31073f01f847c8
Rebecca Fransson <rebecca@kollektiva.se>
2017-08-20 21:52:15 +0200

    more stuff

---
+++ dist/src/components/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <navigation></navigation>
+    <bottomBar></bottomBar>
+  </div>
+</template>
+
+<script>
+import Navigation from './Navigation.vue'
+import BottomBar from './BottomBar'
+
+export default {
+  name: 'app',
+  components: {
+    'navigation': Navigation,
+    'bottomBar': BottomBar
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto');
+html {
+  height: 100vh;
+  width: 100vw;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #c4d4dd;
+  width: 100vw;
+  height: 100vh;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+</style>
---
+++ dist/src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="home">
+    <navigation></navigation>
+    
+    <bottomBar></bottomBar>
+    
+  </div>
+</template>
+
+<script>
+import Navigation from './Navigation'
+import BottomBar from './BottomBar'
+
+export default {
+  name: 'home',
+  components: {
+    navigation: Navigation,
+    bottomBar: BottomBar
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto');
+
+body {
+  background-color: #c4d4dd;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
