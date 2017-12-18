f8681cd21545b76fdaed6e38cfcc65747409b879
RebeccaFransson <rebecca@kollektiva.se>
2017-12-18 14:14:25 +0100

    added new pictures to gallery

--- src/components/popups/Gallery.vue
+++ src/components/popups/Gallery.vue
@@ -17,8 +17,24 @@ export default {
     return {
         index: 0,
         images: [
-            '/static/emilhopp.jpg',
-            '/static/alla.jpg'
+            '/static/gallery/farmDucks.jpg',
+            '/static/gallery/horseDioraAndCharlie.jpg',
+            '/static/gallery/horseMidori.jpg',
+            '/static/gallery/pastureEveningSun.jpg',
+            '/static/gallery/pastureEveningSunWithHorses.jpg',
+            '/static/gallery/pastureInTheTent.jpg',
+            '/static/gallery/pastureLotOfHorses.jpg',
+            '/static/gallery/pastureMist.jpg',
+            '/static/gallery/pastureMorningSun.jpg',
+            '/static/gallery/pastureMorningSun2.jpg',
+            '/static/gallery/pastureMorningSunWithHorses.jpg',
+            '/static/gallery/ridingArena.jpg',
+            '/static/gallery/stall.jpg',
+            '/static/gallery/trainingAlbinAndSonja.jpg',
+            '/static/gallery/trainingDioraAndSandra.jpg',
+            '/static/gallery/trainingEmilAndPernilla.jpg',
+            '/static/gallery/trainingErosAndTina.jpg',
+            '/static/gallery/trainingQubbe.jpg'
         ]
     }
   },
@@ -33,13 +49,19 @@ export default {
 }
 
 #gallery .image{
-  background-size: contain;
+  background-size: cover;
+  background-position: center;
   margin: 5px;
+  height: 200px !important;
+  min-width: 200px !important;
+}
+#gallery .image:hover{
+  cursor: pointer;
 }
 
 #gallery img{
-    width: 90%;
-    border: 1px solid black;
+  max-width: 90%;
+  border: 1px solid black;
 }
 
 
