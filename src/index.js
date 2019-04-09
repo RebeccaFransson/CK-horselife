72ab688bf6535bc618cb9f67631ec2fea1af9524
RebeccaFransson <rebecca@kollektiva.se>
2019-04-09 14:15:10 +0200

    bootstrap and jquery works with default html-template from poi

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
+
+// Set jQuery as global variable
+import * as jQuery from 'jquery'
+window.$ = jQuery
+
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
 import App from './components/App.vue'
 
 new Vue({
